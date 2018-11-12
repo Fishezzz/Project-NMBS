8b76e977f0c1cccee3fb2a160e6a376b364c2f01
Fishezzz <emiel.demeestere@gmail.com>
2018-11-12 11:17:03 +0100

    to begin

--- Project NMBS/MainWindow.xaml.cs
+++ Project NMBS/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 using GTFS;
+using GTFS.Attributes;
+using GTFS.Entities;
+using GTFS.Entities.Collections;
+using GTFS.Entities.Enumerations;
+using GTFS.Exceptions;
+using GTFS.Fields;
+using GTFS.Filters;
+using GTFS.IO;
+using GTFS.IO.CSV;
+using GTFS.StopsToShape;
+using GTFS.Validation;
 
 namespace Project_NMBS
 {
